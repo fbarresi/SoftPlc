b056c926d1b3957d2ae5cfadd65a79f7158cb1d2
Federico Barresi <federico.barresi@evopro-ag.de>
2018-06-22 20:52:57 +0200

    Update build_docker_linux.ps1

--- SoftPlc/scripts/build_docker_linux.ps1
+++ SoftPlc/scripts/build_docker_linux.ps1
@@ -1,5 +1,5 @@
-dotnet publish -c release -o release
+dotnet publish -c release /p:DefineConstants=__MonoCS__ -o release_linux
 
-docker build -f .\Docker\linux-x64\Dockerfile -t fbarresi/softplc:latest .
+docker build -f .\Docker\linux-x64\Dockerfile -t fbarresi/softplc:latest-linux .
 
-#docker run -p 5000:80 --name softplc fbarresi/softplc:latest
+#docker run -p 5000:80 -p 102:102 --name softplc fbarresi/softplc:latest
